dc1a5eccfad3b27166330a34211d1003355e3287
alexboliam <alexboliam@gmail.com>
2020-03-20 23:42:28 +0200

    Add comments to Solver

--- Lab1_Uninformative_Search/Solver.cs
+++ Lab1_Uninformative_Search/Solver.cs
@@ -6,25 +6,25 @@ namespace Lab1_Uninformative_Search
 {
     public class Solver
     {
-        public LinkedList<GangStateNode> IDDFS(GangStateNode root)
+        public LinkedList<GangStateNode> IDDFS(GangStateNode root) // метод поиска в глубь с итеративным углублением
         {
-            int depth = 0;
-            while(true)
+            int depth = 0; // сначала глубина равна 0
+            while(true) // проходим по дереву методом ограниченного поиска вглубь, увеличивая постепенно максимальную глубину
             {
-                var result = DLS(root, depth);
-                if (result != null && result.IsSolution())
+                var result = DLS(root, depth); // ищем методом огр. поиска вглубь нашу цель
+                if (result != null && result.IsSolution()) // если она нашлась, восстанавливаем путь и возвращаем его
                     return FindPath(result);
-                depth += 1;
+                depth += 1; // если не нашлась, увеличиваем глубину
             }
         }
-        public GangStateNode DLS(GangStateNode node, int depth)
+        public GangStateNode DLS(GangStateNode node, int depth) // метод поиска вглубь с ограничением глубины
         {
-            if (depth == 0 && node.IsSolution())
+            if (depth == 0 && node.IsSolution()) // проверяем есть ли состояние искомым, когда текущая глубина равна нулю
                 return node;
-            else if (depth > 0)
+            else if (depth > 0) 
             {
-                var moves = node.GetPossibleMoves();
-                foreach (var child in moves)
+                var moves = node.GetPossibleMoves(); // высчитываем все возможные ходы из данного состояния
+                foreach (var child in moves) // рекурсивного проходим по ним до заданной глубины, проверяем на наличие искомого состояния
                 {
                     var result = DLS(child, depth - 1);
                     if (result != null) return result;
@@ -35,7 +35,7 @@ namespace Lab1_Uninformative_Search
             else
                 return null;
         }
-        private LinkedList<GangStateNode> FindPath(GangStateNode solution)
+        private LinkedList<GangStateNode> FindPath(GangStateNode solution) // восстанавливаем путь от найденого состояния до начального
         {
             LinkedList<GangStateNode> path = new LinkedList<GangStateNode>();
 
