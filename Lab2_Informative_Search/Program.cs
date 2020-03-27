801a59fc1d0f71bfe53150bb87c4bcb9a9d430bb
alexboliam <alexboliam@gmail.com>
2020-03-27 19:38:39 +0200

    Add basic table state class

--- Lab2_Informative_Search/Program.cs
+++ Lab2_Informative_Search/Program.cs
@@ -7,6 +7,32 @@ namespace Lab2_Informative_Search
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[][] a = new int[2][];
+            for (int i = 0; i < 2; i++)
+            {
+                a[i] = new int[2];
+                for (int j = 0; j < 2; j++)
+                    a[i][j] = 5;
+            }
+            int[][] b = new int[2][];
+            for (int i = 0; i < 2; i++)
+            {
+                b[i] = new int[2];
+                for (int j = 0; j < 2; j++)
+                    b[i][j] = 3;
+            }
+            int[][] c = new int[2][];
+            for (int i = 0; i < 2; i++)
+            {
+                c[i] = new int[2];
+                for (int j = 0; j < 2; j++)
+                    c[i][j] = 3;
+            }
+
+            TableState<int> table = new TableState<int>(a, b);
+            Console.WriteLine(table.ArraysEquals(a, b));
+            Console.WriteLine(table.ArraysEquals(b, c));
+            Console.WriteLine(table.ToString());
         }
     }
 }
---
+++ Lab2_Informative_Search/TableState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab2_Informative_Search
+{
+    public class TableState<T> where T:IComparable
+    {
+        public T[][] CurrentTable { get; private set; }
+        public T[][] TargetTable { get; private set; }
+        public TableState<T> Parent { get; private set; }
+        public List<TableState<T>> Moves { get; private set; }
+        public int Heuristic { get; private set; } = 0;
+
+        public TableState(T[][] CurrentTable, T[][] TargetTable, TableState<T> Parent = null)
+        {
+            this.CurrentTable = this.CopyTable(CurrentTable);
+            this.TargetTable = this.CopyTable(TargetTable);
+            this.Parent = Parent;
+            CalcHeuristic();
+        }
+
+
+        #region Private methods
+        private T[][] CopyTable(T[][] table)
+        {
+            return table.Select(x => x.ToArray()).ToArray();
+        }
+        private void CalcHeuristic()
+        {
+            for (int i = 0; i < CurrentTable.Length; i++)
+            {
+                for (int j = 0; j < CurrentTable[i].Length; j++)
+                {
+                    if (!CurrentTable[i][j].Equals(TargetTable[i][j]))
+                        Heuristic++;
+                }
+            }
+        }
+        #endregion
+
+        #region Public methods
+        public bool ArraysEquals(T[][] table1, T[][] table2)
+        {
+            if(table1 == null || table2 == null || table1.Length != table2.Length || table1[0].Length != table2[0].Length)
+            {
+                return false;
+            }
+            for(int i = 0; i<table1.Length;i++)
+            {
+                for (int j = 0; j < table1.Length; j++)
+                {
+                    if (!table1[i][j].Equals(table2[i][j]))
+                        return false;
+                }
+            }
+            return true;
+        }
+        public override string ToString()
+        {
+            var result = String.Empty;
+            result += $"Wrong positions: {Heuristic}\n";
+
+            foreach(var item in CurrentTable)
+            {
+                foreach(var subitem in item)
+                {
+                    result += subitem.ToString() + " ";
+                }
+                result += "\n";
+
+            }
+            return result;
+        }
+        #endregion
+    }
+}
