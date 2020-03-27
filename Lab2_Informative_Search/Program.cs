723fb68c0a1f247920e1d14ae4bef580138d7720
alexboliam <alexboliam@gmail.com>
2020-03-27 17:05:20 +0200

    Init lab 2 upload

---
+++ Lab2_Informative_Search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_Informative_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
